f19faa4aadbbeea9747aa302c027973b575a7818
osuke <arget-fee.free.dgm@hotmail.co.jp>
2019-01-06 14:41:21 +0900

    impl mint_applem, set_price, ownership

--- apple-store/runtime/src/store.rs
+++ apple-store/runtime/src/store.rs
@@ -7,29 +7,58 @@ pub trait Trait: balances::Trait {
     type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
 
+
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
         fn deposit_event() = default;
 
-        fn buy_apple() -> Result {
+        fn buy_apple(origin, nums: T::Balance) -> Result {
+            let sender = ensure_signed(origin)?;  
+            let cur_apple_stock_amount = Self::apple_stock_amount();
 
+            ensure!(
+                cur_apple_stock_amount >= Some(nums),
+                "I am sorry but apples are sold out |o|",
+            );
+            <balances::Module<T>>::decrease_free_balance(&sender, Self::apple_price * nums)?;
+
+            
+
+            let cur_apple_nums = Self::apple_of(sender.clone());
+            <AppleOf<T>>::insert(sender, cur_apple_nums + nums);
+            <AppleStockAmount<T>>:put(cur_apple_stock_amount - nums);
+            Ok(())
         }
 
-        fn sell_apple() -> Result {
-
+        fn sell_apple(origin, nums: u32) -> Result {
+            let sender = ensure_signed(origin)?;
+            Ok(())
         }
 
-        fn mint_apple() -> Result {
+        fn mint_apple(mint_by: T::Balance) -> Result {
+            let sender = ensure_signed(origin)?;
+            ensure!(sender == Self::owner(), "Only owner can mint apples.");           
 
+            <AppleAmount<T>>::mutate(|amount| {
+                let new_amount = amount.map_or(mint_by, |amount| amount + mint_by);
+                *amount = Some(new_amount);
+            });            
+            Ok(())
         }
 
-        fn set_value() -> Result {
+        fn set_price(origin, price: u32) -> Result {
+            let sender = ensure_signed(origin)?;            
+            ensure!(sender == Self::owner(), "Only owner can set the price of an apple.")
 
+            <ApplePrice<T>>::put(price);
+            Self::deposit_event(RawEvent::ApplePriceSet(price));
+            Ok(())
         }
 
         fn init_ownership(origin) -> Result {
-            ensure!(!<Owner<T>>::exists(), "Owner already exists");
+            ensure!(!<Owner<T>>::exists(), "Owner already exists");            
             let sender = ensure_signed(origin)?;
+
             <Owner<T>>::put(&sender);
             Self::deposit_event(RawEvent::OwnershipTransferred(sender.clone(), sender));
             Ok(())
@@ -38,6 +67,7 @@ decl_module! {
         fn transfer_ownership(origin, newOwner: T::AccountId) -> Result {
             let sender = ensure_signed(origin)?;
             ensure!(sender == Self::owner(), "This function can only be called by the owner");
+
             <Owner<T>>::put(&newOwner);
             Self::deposit_event(RawEvent::OwnershipTransferred(sender, newOwner));
             Ok(())
@@ -48,12 +78,16 @@ decl_module! {
 
 decl_storage! {
     trait Store for Module<T: Trait> as AppleStore {
-        Owner get(owner): T::AccountId;
+        pub Owner get(owner): T::AccountId;
+        pub AppleOf get(apple_of): map T::AccountId => Option<T::Balance>;
+        pub ApplePrice get(apple_price): u32;
+        pub AppleStockAmount get(apple_stock_amount): Option<T::Balance>;
     }
 }
 
 decl_event! {
     pub enum Event<T> where AccountId = <T as system::Trait>>::AccountId {
         OwnershipTransferred(AccountId, AccountId),
+        ApplePriceSet(u32),
     }
 }
